77ad827cc5923533c745f41a9bc7b0255a908548
j-kayes <jkayes2718@gmail.com>
2019-04-01 22:31:10 +0100

    Appears to train on breakout.

--- agent.py
+++ agent.py
@@ -9,6 +9,7 @@ import keras.backend as K
 import cv2
 from keras.models import Model
 from keras.layers import Dense, Dropout, Input
+from keras.optimizers import Adam
 from statistics import mean, median
 
 def squared_difference(y_true, y_pred): # Loss function
@@ -34,7 +35,7 @@ class Agent:
         self.lr = learning_rate
 
         # Input to the model is a sequence of scaled down screen states:
-        self.input_shape = (trace_size, 84, 84)
+        self.input_shape = (84, 84, trace_size)
 
         self.build_model()
 
@@ -42,23 +43,22 @@ class Agent:
         self.x_input = Input(shape=self.input_shape, name='x')
 
         hidden_1 = keras.layers.Conv2D(16, (8,8), strides=(4, 4), activation='relu')(self.x_input)
-        hidden_2 = keras.layers.Conv2D(32, (4,4), strides=(2, 2), activation='relu')(hidden_2)
+        hidden_2 = keras.layers.Conv2D(32, (4,4), strides=(2, 2), activation='relu')(hidden_1)
 
-        hidden_3 = keras.Dense(256, activation='relu')
+        flattened = keras.layers.Flatten()(hidden_2)
+        hidden_3 = keras.layers.Dense(256, activation='relu')(flattened)
 
         # Different output for each action:
         self.y_outputs = []
-        for action_index in range(self.env.action_space.n):
-            self.y_outputs.append(Dense(1)(hidden_layer))
+        for action_q in range(self.env.action_space.n):
+            self.y_outputs.append(Dense(1)(hidden_3))
 
         self.model = Model(inputs=self.x_input, outputs=self.y_outputs)
 
-        #self.model.summary()
         self.model.compile(loss=squared_difference,
-            loss_weights=[1.0 for n in range(self.env.action_space.n)],
-            metrics=['accuracy'],
-            optimizer='adam')
-        K.set_value(self.model.optimizer.lr, self.lr)
+                    loss_weights=[1.0 for n in range(self.env.action_space.n)],
+                    metrics=['accuracy'],
+                    optimizer=Adam(lr=self.lr))
 
     def process_sequence(self, sequence_data):
         # Down samples and processes a sequence of data in a memory trace of the
@@ -74,10 +74,12 @@ class Agent:
         # Remove colour:
         sequence_trace = np.mean(sequence_trace, axis=3, keepdims=False)
         # Down sample:
-        downsampled_trace = []
+        downsampled_trace = np.empty((84,84,len(sequence_trace)))
+        index = 0
         for state_image in sequence_trace:
             resized = cv2.resize(state_image, (84, 110), interpolation=cv2.INTER_AREA)
-            downsampled_trace.append(resized[18:102, :])
+            downsampled_trace[:, :, index] = resized[18:102, :]
+            index += 1
 
         return np.array(downsampled_trace).astype(np.uint8)
 
@@ -86,15 +88,13 @@ class Agent:
 
     def get_best_q_value(self, x_input, batch_index=0):
         full_output = self.model.predict(x_input)
-        print(full_output)
-        exit()
 
         best_q = None
-        for output_batches in full_output:
+        for action_q_batch in full_output:
             if(best_q is None):
-                best_q = output_batches
-            elif(output_batches > best_q):
-                best_q = output_batches
+                best_q = action_q_batch[batch_index]
+            elif(action_q_batch > best_q):
+                best_q = action_q_batch[batch_index]
 
         return best_q
 
@@ -105,12 +105,12 @@ class Agent:
         action_n = 0
         best_action = None
         best_q = None
-        for output_batches in full_output:
+        for output_q_batches in full_output:
             if(best_q is None):
-                best_q = output_batches[batch_index]
+                best_q = output_q_batches[batch_index]
                 best_action = action_n
-            elif(output_batches[batch_index] > best_q):
-                best_q = output_batches[batch_index]
+            elif(output_q_batches[batch_index] > best_q):
+                best_q = output_q_batches[batch_index]
                 best_action = action_n
 
             action_n += 1
@@ -118,7 +118,7 @@ class Agent:
         return best_action
 
     def get_samples(self, stop_after_limit=True, n_games=500000, max_t=250,
-    epsilon=1.0, display_frames=False):
+        epsilon=1.0, display_frames=False):
         # Will append the memory with state/action/reward data and return the
         # average score:
         game_counter = 0
@@ -134,12 +134,13 @@ class Agent:
             for t in range(max_t):
                 frames += 1
                 action = None
-                processed_current_state = self.process_sequence(sequence)
+                processed_current_state = np.array([self.process_sequence(sequence)])
+                assert(processed_current_state.shape is not (1,84,84,4))
                 # We start with a high epsilon:
                 if(random.random() < epsilon):
                     action = self.env.action_space.sample()
                 else:
-                    # Best action for this processed sequence(acording to the model):
+                    # Best action for this processed sequence:
                     action = self.get_best_action(processed_current_state)
 
                 # Get the reward/state information after taking this action:
@@ -150,8 +151,6 @@ class Agent:
                 if(not done):
                     sequence.append(next_state)
                 processed_next_state = self.process_sequence(sequence)
-                if(t == 200):
-                    cv2.imwrite(str(game) +'-frame-' + str(t) + '.png', processed_next_state[0])
                 # Append to memory (up to limit):
                 if(len(self.memory) < self.memory_size):
                     self.memory.append((processed_current_state, action, reward, processed_next_state, done))
@@ -177,18 +176,17 @@ class Agent:
     gamma=0.95, score_sample_size=250):
         # Trains the agent.
         # Play randomly until memory has at least batch_size entries
-        print("Training")
         while(len(self.memory) < batch_size):
             self.get_samples(False, 1, epsilon=1.0)
         total_frames = 0
         scores = []
         for game in range(games):
-            # Scales linearly from initial to final epsilon up to epsilon frames range:
+            # Scales linearly from initial to final epsilon:
             if(total_frames < epsilon_frames_range):
                 e = initial_epsilon - ((initial_epsilon - final_epsilon) * (total_frames / epsilon_frames_range))
             else:
                 e = final_epsilon
-            # Play a random game, and record data to memory buffer:
+            # Play a  game, and record data to memory buffer:
             score, frames = self.get_samples(False, 1, epsilon=e)
             scores.append(score)
             if(len(scores) == score_sample_size):
@@ -198,7 +196,7 @@ class Agent:
             m_score = mean(scores)
             total_frames += frames
             print('Game: {} Score: {}, Mean: {:.2f} Frames: {} e: {:.4f}'.format(
-            game, score, m_score, total_frames, e))
+            game, score, m_score, frames, e))
 
             if((m_score >= target_mean_score) and (len(scores) > 200)):
                 print('Target mean score reached')
@@ -215,14 +213,12 @@ class Agent:
                     target = reward + gamma*self.get_best_q_value(p_next_state)
 
                 # Predicted Q-values for each action according to the model:
-                q_values = self.model.predict(p_state, batch_size=1)
-                actual = self.model.predict(p_state, batch_size=1)
+                q_values = self.model.predict(p_state[0])
+                actual = self.model.predict(p_state[0])
                 q_values[action] = np.array([target]) # With updated target.
 
-                #print(K.get_value(self.model.optimizer.lr))
-
                 # Try to fit to the target Q-values for each action:
-                self.model.train_on_batch(p_state, q_values)
+                self.model.train_on_batch(p_state[0], q_values)
 
         print('Training complete')
         # TODO: Model saving:
@@ -245,7 +241,7 @@ class Agent:
             med_score = median(scores)
 
             total_frames += frames
-            print('Games: {} Score: {}, Mean Score: {:.2f}, Median: {:.2f} Total Frames: {}'.format(
+            print('Games: {} Score: {}, Mean Score: {:.2f}, Median: {:.2f}, Total Frames: {}'.format(
                     game_num,
                     score,
                     m_score,
